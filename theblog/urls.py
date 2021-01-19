5a087ab641975a7e6bd9befa25c7cc619992e822
mostofa247 <74599998+mostofa247@users.noreply.github.com>
2021-01-19 13:25:34 -0500

    New adittion

--- theblog/urls.py
+++ theblog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import HomeView,A_DetailView
+from .views import HomeView,A_DetailView,AddPostView
 
 urlpatterns = [
     #path('',views.home, name="home"),
     path('',HomeView.as_view(),name="home"),
     path('details/<int:pk>',A_DetailView.as_view(),name='details'),
+    path('add_post/',AddPostView.as_view(),name='add_post'),
 ]
